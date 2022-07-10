76ad239f478c8feb538249de4ac77dd7ab84e76e
Ashley Raiteri <a.raiteri@outlook.com>
2022-07-10 19:43:34 +1000

    Changing RNG publisher's logging function to use Python's native logging module.

--- rng_publisher/rng_publisher.py
+++ rng_publisher/rng_publisher.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import logging
 import random
 import time
 import json
@@ -8,6 +10,7 @@ import paho.mqtt.client as mqtt
 
 
 # Get script vars from env
+logging_level = str(os.getenv('LOGGING_LEVEL'))
 host = str(os.getenv('BROKER_HOST'))
 port = int(os.getenv('BROKER_PORT'))
 client_id = str(os.getenv('CLIENT_ID'))
@@ -15,40 +18,54 @@ clean_session = bool(os.getenv('CLEAN_SESSION'))  # True/false (control whether 
 username = str(os.getenv('USERNAME'))  # MQTT username/password authentication
 password = str(os.getenv('PASSWORD'))  # MQTT username/password authentication
 rng_topic = str(os.getenv('RNG_TOPIC'))  # Topic to publish RNG values on.
+log_format = "%(levelname)s %(asctime)s - %(message)s"
+logging_levels = {
+    'debug': logging.DEBUG,
+    'info': logging.INFO,
+    'warning': logging.WARNING,
+    'error': logging.ERROR,
+    'critical': logging.CRITICAL,
+}
+if logging_level not in logging_levels.keys():
+    raise Exception("Invalid logging level received: '{0}'. Please provide one of the following as logging level: "
+                    "debug, info, warning, error, critical.".format(str(logging_level)))
+logging.basicConfig(stream=sys.stdout,
+                    format=log_format,
+                    level=logging_levels[logging_level])
 while True:
     c = mqtt.Client(
         client_id=client_id,
         clean_session=clean_session)
     try:
-        print("Connecting to MQTT broker '{0}' with client ID '{1}'...".format(host, client_id))
-        print("Initiating a clean session: {0}".format(str(clean_session)))
-        print("Username '{0}' and password '{1}' set.".format(username, password))
         c.username_pw_set(
             username=username,
             password=password)
+        logging.info("Attempting connection to MQTT broker '{0}'...".format(host))
         c.connect(
             host=host,
             port=port)
-        print("Successfully connected.")
+        logging.info("Successfully connected to MQTT broker '{0}'.".format(host))
         print("Starting publish stream of RNG values...")
         while True:
             try:
                 curr_tstamp = datetime.timestamp(datetime.now())
                 rng_value = str(random.choice(range(1, 100)))
-                print("RNG value to publish to broker is {0}.".format(rng_value))
+                logging.info("RNG value to publish to broker is {0}.".format(rng_value))
                 payload = {'timestamp': curr_tstamp, 'rng_value': rng_value}
                 c.publish(
                     topic=rng_topic,
                     payload=json.dumps(payload))
-                print("Successfully published RNG value {0} to MQTT broker '{1}', topic '{2}'.".format(rng_value, host, rng_topic))
+                logging.info("Successfully published RNG value {0} to MQTT broker '{1}', "
+                             "topic '{2}'.".format(rng_value, host, rng_topic))
                 time_till_next_message = random.choice(range(1, 30))
-                print("Sleeping for {0} seconds before publishing again...".format(str(time_till_next_message)))
+                logging.info("Sleeping for {0} seconds before publishing again...".format(str(time_till_next_message)))
                 time.sleep(time_till_next_message)
             except Exception as e:
-                traceback.print_exc()
+                logging.error(traceback.format_exc())
     except Exception as e:
-        traceback.print_exc()
+        logging.error(traceback.format_exc())
     finally:
-        print("Disconnecting from MQTT broker '{0}'...".format(host))
+        print("RNG value publishing stopped.")
+        logging.info("Disconnecting from MQTT broker '{0}'...".format(host))
         c.disconnect()
-        print("Successfully disconnected.")
+        logging.info("Successfully disconnected from MQTT broker '{0}'.".format(host))
